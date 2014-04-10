50824b9558674f65ad308a5f39d7e03ac1e1a749
benelliott <bge23@cam.ac.uk>
2014-04-10 19:46:18 +0100

    Added untracked abstract filter and windowing classes.

---
+++ SpectrogramAndroid/src/uk/co/benjaminelliott/spectrogramandroid/BandpassFilter.java
@@ -0,0 +1,7 @@
+package uk.co.benjaminelliott.spectrogramandroid;
+
+public abstract class BandpassFilter {
+	
+	abstract void applyFilter(short[] samples);
+
+}
---
+++ SpectrogramAndroid/src/uk/co/benjaminelliott/spectrogramandroid/WindowFunction.java
@@ -0,0 +1,7 @@
+package uk.co.benjaminelliott.spectrogramandroid;
+
+public abstract class WindowFunction {
+
+	
+	abstract void applyWindow(double[] samples);
+}
